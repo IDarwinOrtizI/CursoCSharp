ceb1fa374f9578edf82d3f1264aa8d0d330e85fb
IDarwinOrtizI <sebastiancaballero0811@gmail.com>
2025-08-12 12:41:06 -0500

    first commit

---
+++ 1Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string miCadena = "Hola, esto es una cadena";
+Console.WriteLine(miCadena);
+
+miCadena = string.Empty;
+Console.WriteLine(miCadena);
+
+/*
+Console.WriteLine("Digite su nombre por consola.. ");
+string tuNombre = Console.ReadLine()!;
+
+Console.WriteLine("Digite su edad: ");
+string entradaEdad = Console.ReadLine()!;
+int edad = Convert.ToInt32(entradaEdad);
+Console.WriteLine("Tu edad es: " + edad);
+
+Console.WriteLine("Tu nombre es: " + tuNombre + " y tu edad es: " + edad);
+*/
+
+string nombre = "Sebastian";
+string apellido = "Ortiz";
+Console.WriteLine("Tu nombre es: " + nombre + " " + apellido);
